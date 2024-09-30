83b75488b9bc7bb942902cadd223cf5d57108052
Harsh jha <harsh1998mrperfect@gmail.com>
2024-09-30 10:07:30 +0530

    2nd lecture finished, added parcel, added react and react-dom through npm insted of cdn link

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
-// const rootel = React.createElement(
-//   "h1",
-//   { id: "heading", creator: "Harsh jha" },
-//   "hello from react"
-// );
-
-/*
-    div "paernt"
-        div "child1"
-            h1 "(hello form h1 element)"
-            h2 "(hello form h2 element)"
-        div
-        div "child2"
-            h1 "(hello form h1 element)"
-            h2 "(hello form h2 element)"
-        div
-    div
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "hello form h1 element"),
